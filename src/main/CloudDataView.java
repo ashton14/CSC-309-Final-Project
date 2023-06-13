74e5f1be26ea34f8f340d136ef38fd8e580ecb9e
connor-addison-h <connorahickey@gmail.com>
2023-06-12 20:13:52 -0700

    JavaDocs

--- src/main/CloudDataView.java
+++ src/main/CloudDataView.java
@@ -7,11 +7,28 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.util.List;
 
+/**
+ * The CloudDataView class represents a view that displays the diagrams saved in the cloud.
+ * It implements the AppPage interface.
+ * It provides methods to show the contents of the view, retrieve header information, and handle diagram button clicks.
+ *
+ * The diagrams are retrieved from the cloud.
+ * The view is displayed as a vertical list of diagram buttons.
+ * Clicking on a diagram button opens the DiagramApp and displays the selected diagram.
+ *
+ * @author Alex Banham, Connor Hickey
+ */
 public class CloudDataView extends JPanel implements AppPage {
 
     private TeachingApp teachingApp;
     private List<String> diagramNames;
 
+    /**
+     * Constructs a CloudDataView object with the specified TeachingApp and list of diagram names.
+     *
+     * @param teachingApp the TeachingApp instance.
+     * @param diagramNames the list of diagram names.
+     */
     public CloudDataView(TeachingApp teachingApp, List<String> diagramNames) {
         this.teachingApp = teachingApp;
         this.diagramNames = diagramNames;
@@ -55,6 +72,11 @@ public class CloudDataView extends JPanel implements AppPage {
         this.add(scrollPane, BorderLayout.CENTER);
     }
 
+    /**
+     * Displays the selected diagram by downloading it from the cloud and reading it into the DiagramApp.
+     *
+     * @param filename the filename of the selected diagram.
+     */
     private void displayDiagram(String filename){
         SqlControlHandler.downloadFlowchart(filename);
         String trimmedStr = filename.substring(0, filename.length() - 5);
@@ -65,20 +87,39 @@ public class CloudDataView extends JPanel implements AppPage {
         teachingApp.setVisible(false);
     }
 
+    /**
+     * Shows the contents of the CloudDataView by making it visible.
+     */
     @Override
     public void showContents() {
         this.setVisible(true);
     }
 
+    /**
+     * Retrieves the header information of the CloudDataView.
+     * The header information consists of the string "Cloud Saved Diagrams".
+     *
+     * @return the string "Cloud Saved Diagrams".
+     */
     @Override
     public String getHeaderInfo() {
         return "Cloud Saved Diagrams";
     }
 
+    /**
+     * Retrieves the diagram names of the CloudDataView.
+     *
+     * @return a list of the diagram names.
+     */
     public List<String> getDiagramNames() {
         return diagramNames;
     }
 
+    /**
+     * Sets the diagram names of the CloudDataView.
+     *
+     * @param diagramNames the list of diagram names.
+     */
     public void setDiagramNames(List<String> diagramNames) {
         this.diagramNames = diagramNames;
     }
